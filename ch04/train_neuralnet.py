cc95daf902af1daa945b1e559de0fccc9fb446e1
Horevl <xj806411orevl@163.com>
2024-09-27 15:24:29 +0800

    2024/9/27 15:24

--- ch04/train_neuralnet.py
+++ ch04/train_neuralnet.py
@@ -11,10 +11,11 @@ from two_layer_net import TwoLayerNet
 
 network = TwoLayerNet(input_size=784, hidden_size=50, output_size=10)
 
+# 超参数
 iters_num = 10000  # 适当设定循环的次数
 train_size = x_train.shape[0]
 batch_size = 100
-learning_rate = 0.1
+learning_rate = 0.1 # lr
 
 train_loss_list = []
 train_acc_list = []
@@ -23,6 +24,7 @@ test_acc_list = []
 iter_per_epoch = max(train_size / batch_size, 1)
 
 for i in range(iters_num):
+    # 获取mini-batch
     batch_mask = np.random.choice(train_size, batch_size)
     x_batch = x_train[batch_mask]
     t_batch = t_train[batch_mask]
@@ -35,9 +37,11 @@ for i in range(iters_num):
     for key in ('W1', 'b1', 'W2', 'b2'):
         network.params[key] -= learning_rate * grad[key]
     
+    # 记录学习过程
     loss = network.loss(x_batch, t_batch)
     train_loss_list.append(loss)
     
+    # 计算每个epoch的识别精度
     if i % iter_per_epoch == 0:
         train_acc = network.accuracy(x_train, t_train)
         test_acc = network.accuracy(x_test, t_test)
